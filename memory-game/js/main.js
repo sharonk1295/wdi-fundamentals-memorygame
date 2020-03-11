559a378192453f80090f578d5a129abbbc9877ee
Caroline Kim <carolinek@SKs-MacBook-Air.local>
2020-03-11 09:49:36 -0400

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,17 +1,20 @@
 console.log("Up and running!");
-//console.log("User flipped " + cardThree);
 let cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-if (cardsInPlay.length === 2) {
+
+function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
+  console.log("You found a match!");
 	} else {
-		alert("Sorry, try again.");
+  	console.log("Sorry, try again.");
 	}
 };
+function flipCard(cardId) {	
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);
+

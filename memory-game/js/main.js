6fa3dc7e81121ce1997adbb6c6650ef59b92ae54
Caroline Kim <carolinek@SKs-MacBook-Air.local>
2020-03-11 21:30:39 -0400

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,5 +1,26 @@
 console.log("Up and running!");
-let cards = ["queen", "queen", "king", "king"];
+let cards = [
+{
+	rank : "queen",
+	suit : "hearts",
+	cardImage : "images/queen-of-hearts.png"
+},
+{
+	rank : "queen",
+	suit : "diamonds",
+	cardImage : "images/queen-of-diamonds.png"
+},
+{
+	rank : "king",
+	suit : "hearts",
+	cardImage : "images/king-of-hearts.png"
+},
+{
+	rank : "king",
+	suit : "diamonds",
+	cardImage : "images/king-of-diamonds.png"
+}
+];
 let cardsInPlay = [];
 
 function checkForMatch() {
@@ -10,11 +31,21 @@ function checkForMatch() {
 	}
 };
 function flipCard(cardId) {	
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
-	checkForMatch();
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+		}
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
 };
 
 flipCard(0);
+console.log(cards[0].cardImage);
+console.log(cards[0].suit);
 flipCard(2);
-
+console.log(cards[2].cardImage);
+console.log(cards[2].suit);
